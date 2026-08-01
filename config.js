764f65fc2e518f81051bf85462a0f7354925d3ff
san01446-ux <san01446@gmail.com>
2026-08-02 06:32:14 +0900

    s

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ABADDON_CONFIG = Object.freeze({
-  version: "v6.4.0a",
+  version: "v6.4.0b",
   statusText: "AWAITING SIGNAL",
-  statusNote: "DARKZONE · SUPPLY FEVER · MAIL & ALERTS · v6.4.0a",
+  statusNote: "DARKZONE · SUPPLY FEVER · MAIL & ALERTS · v6.4.0b",
   discordInvite: "https://discord.gg/FN2tX7TVMz",
   botInvite: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
 
