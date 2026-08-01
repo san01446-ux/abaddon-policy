d7b3a054b3f42829c05f838af2e4dc78a77a9639
san01446-ux <san01446@gmail.com>
2026-08-02 03:30:27 +0900

    homepage

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ABADDON_CONFIG = Object.freeze({
-  version: "v6.3.5",
+  version: "v6.3.5c",
   statusText: "AWAITING SIGNAL",
-  statusNote: "CASINO REACTIONS · FIVE-STAGE BASE · v6.3.5",
+  statusNote: "WEAPON & RELIC RENEWAL · CLEAN CASINO UI · v6.3.5c",
   discordInvite: "https://discord.gg/FN2tX7TVMz",
   botInvite: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
 
