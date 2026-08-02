c52e58c3b9c4d74fa37eac5eaac8c1a652b85003
san01446-ux <san01446@gmail.com>
2026-08-02 09:07:08 +0900

    s

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ABADDON_CONFIG = Object.freeze({
-  version: "v6.5.0",
+  version: "v6.5.1",
   statusText: "AWAITING SIGNAL",
-  statusNote: "VISUAL REMASTER · 28 THEMES · RESPONSIVE UI · v6.5.0",
+  statusNote: "28 THEMES · SERVER RENEWAL · CARD GAMES · 2026-08-02",
   discordInvite: "https://discord.gg/FN2tX7TVMz",
   botInvite: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
 
