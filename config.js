7311e772a7214399d269e1835c8e5a8fca99058f
san01446-ux <san01446@gmail.com>
2026-08-03 17:39:22 +0900

    6

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ABADDON_CONFIG = {
-  version: "v10.6.0",
+  version: "v10.9.0",
   status: "ONLINE",
-  statusNote: "실전 카드게임 16종 · 섯다 · 실제 고스톱/맞고 · 음수 잔액 · 무상한 · 2026-08-03",
+  statusNote: "카드게임 25종 · 전 종목 ABADDON · 결과/잔액 정산 · 카드룸/관전/리플레이 · 대시보드 · 리그 · 2026-08-03",
   botInviteUrl: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
   discordUrl: "https://discord.gg/FN2tX7TVMz"
 };
