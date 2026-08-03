cf9f01925e94b25bdb539a531a75a84158d52fda
san01446-ux <san01446@gmail.com>
2026-08-03 10:34:26 +0900

    123

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ABADDON_CONFIG = Object.freeze({
-  version: "v8.1.1",
+  version: "v9.0.0",
   statusText: "AWAITING SIGNAL",
-  statusNote: "WORLD MAP · FRONTIER · TERMINAL · 2026-08-03",
+  statusNote: "FACTIONS · TRADE · WAR · SEASON 5 · 2026-08-03",
   discordInvite: "https://discord.gg/FN2tX7TVMz",
   botInvite: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
 
