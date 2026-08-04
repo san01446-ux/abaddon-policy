1456e227ac35761aa37c49031ce35ff39cea6f9f
san01446-ux <san01446@gmail.com>
2026-08-05 01:57:29 +0900

    55

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ABADDON_CONFIG = {
-  version: "v12.2.0",
+  version: "v13.2.0",
   status: "ONLINE",
-  statusNote: "혼돈의 축제 · 파티게임 · 동료 · 탐험 · 사업 · 2026-08-05",
+  statusNote: "BLACK CITY · 세력 · 제작 · 거래소 · 월드 시즌 · 2026-08-05",
   botInviteUrl: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
   discordUrl: "https://discord.gg/FN2tX7TVMz",
   eventFeedUrl: "",
