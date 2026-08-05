60f3881a0e41f6de045487eb74e61d2fe77639e4
san01446-ux <san01446@gmail.com>
2026-08-05 10:24:49 +0900

    4

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ABADDON_CONFIG = {
-  version: "v16.2.1",
+  version: "v16.3.0",
   status: "ONLINE",
-  statusNote: "한글 폰트 수정 · 명령어 그룹 축소 · 홈페이지 빠른 이동 · 2026-08-05",
+  statusNote: "메인 RPG 시즌 1~5 · 전체 명령 자동 분류 · 도시 공방 20종 · 자동 이모지 확장 · 2026-08-05",
   botInviteUrl: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
   discordUrl: "https://discord.gg/FN2tX7TVMz",
   eventFeedUrl: "",
--- en/config.js
+++ en/config.js
@@ -1,5 +1,5 @@
 window.ABADDON_CONFIG = {
-  version: "v16.2.1",
+  version: "v16.3.0",
   status: "ONLINE",
   statusNote: "Visual font fix · compact command groups · quick-jump homepage · 2026-08-05",
   botInviteUrl: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
