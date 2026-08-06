2f712b160a12f6af56a80a5b3bb6ff772ae685b3
san01446-ux <san01446@gmail.com>
2026-08-06 15:54:17 +0900

    5

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ABADDON_CONFIG = {
-  version: "v17.0.0",
+  version: "v17.2.0",
   status: "ONLINE",
-  statusNote: "BLACK SUN CREATOR ERA · 콘텐츠 공방 · 시즌 6 · 2026-08-06",
+  statusNote: "LIVING WORLD & BROKEN OATHS · 살아 있는 세계 · NPC 인연 · 2026-08-06",
   botInviteUrl: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
   discordUrl: "https://discord.gg/FN2tX7TVMz",
   eventFeedUrl: "",
