4a73712b4d5fa49fbebefe18a56fb04a50bc5044
san01446-ux <san01446@gmail.com>
2026-08-06 16:29:01 +0900

    5

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ABADDON_CONFIG = {
-  version: "v17.2.0",
+  version: "v17.3.0",
   status: "ONLINE",
-  statusNote: "LIVING WORLD & BROKEN OATHS · 살아 있는 세계 · NPC 인연 · 2026-08-06",
+  statusNote: "CONNECTED SURVIVAL LOOP · 연결 생존 허브 · 일일 목표 · 2026-08-06",
   botInviteUrl: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
   discordUrl: "https://discord.gg/FN2tX7TVMz",
   eventFeedUrl: "",
