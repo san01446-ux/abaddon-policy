c11648665b62d0de1630323ea59db485c8e28cbb
san01446-ux <san01446@gmail.com>
2026-08-07 07:54:15 +0900

    1

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ABADDON_CONFIG = {
-  version: "v18.0.0",
+  version: "v18.0.1",
   status: "ONLINE",
-  statusNote: "FINAL ECLIPSE · DEFINITIVE EDITION · HOTFIX-ONLY PRESERVATION · 2026-08-07",
+  statusNote: "FINAL ECLIPSE · WEBSITE CLEAN EDITION · 2026-08-07",
   botInviteUrl: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
   discordUrl: "https://discord.gg/FN2tX7TVMz",
   eventFeedUrl: "",
