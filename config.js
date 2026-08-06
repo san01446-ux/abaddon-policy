2e60a28efa97096a74413e931cedec080ea4f81f
san01446-ux <san01446@gmail.com>
2026-08-07 05:59:15 +0900

    5

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.ABADDON_CONFIG = {
-  version: "v17.6.0",
+  version: "v18.0.0",
   status: "ONLINE",
-  statusNote: "CHRONICLE MUSEUM · COMMUNITY SEASON · 장기 생존 경쟁 · 2026-08-07",
+  statusNote: "FINAL ECLIPSE · DEFINITIVE EDITION · HOTFIX-ONLY PRESERVATION · 2026-08-07",
   botInviteUrl: "https://discord.com/oauth2/authorize?client_id=1532237253944934431&permissions=8&integration_type=0&scope=bot+applications.commands",
   discordUrl: "https://discord.gg/FN2tX7TVMz",
   eventFeedUrl: "",
